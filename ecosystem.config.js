6f4d68b8c484b5a6f538ee4e10c529b478dd688f
hugzenfeu <casanave.hugo@gmail.com>
2024-12-03 11:24:27 +0100

    ajout du backup auto tout les jours à minuit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,19 @@ module.exports = {
     },
     {
       name: 'updateDNS',
-      script: './update-dynhost.sh', // Specify the path to your bash script
+      script: './scripts/update-dynhost.sh', // Specify the path to your bash script
       cron_restart: '*/1 * * * *', // This will restart the script every minute
       watch: false, // Disable watching for changes (since this is a bash script)
       autorestart: false,
       env_file: '.env',
-    }
+    },
+    {
+      name: 'backups',
+      script: "./scripts/backups.sh",
+      cron_restart: '0 0 * * *',
+      watch: false,
+      autorestart: false,
+      env_file: '.env',
+    },
   ],
 };
---
+++ scripts/backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+cd ..
+# Load environment variables and run the script
+set -a            
+source .env
+set +a
+
+# Check if required environment variables are set
+if [ -z "$BACKUP_DIR" ] || [ -z "$DYNHOST_SUBDOMAIN" ]; then
+  echo "Error: Required environment variables are not set."
+  echo "Please ensure that BACKUP_DIR and DYNHOST_SUBDOMAIN are defined in the .env file."
+  exit 1
+fi
+
+npm run strapi export -- --file "${BACKUP_DIR}${DATE}${DYNHOST_SUBDOMAIN}.tar.gz" --no-encrypt
+
+# Check if the backup was successful
+if [ $? -eq 0 ]; then
+  echo "Backup created successfully: ${BACKUP_DIR}${DATE}${DYNHOST_SUBDOMAIN}.tar.gz"
+else
+  echo "Error: Backup failed."
+  exit 1
+fi
+
+# Delete backups older than 2 months (60 days)
+find "$BACKUP_DIR" -type f -name "*.tar.gz" -mtime +60 -exec rm -f {} \;
+
+# Check if old backups were deleted successfully
+if [ $? -eq 0 ]; then
+  echo "Old backups older than 2 months have been deleted successfully."
+else
+  echo "Error: Failed to delete old backups."
+  exit 1
+fi
